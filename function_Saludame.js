97ea7753de11f6b43f3d7d4c1086e0ad3e508dec
RaivanLopez <110791119+RaivanLopez@users.noreply.github.com>
2022-08-07 20:40:11 -0300

    Cambio de funcion

--- function_Saludame.js
+++ function_Saludame.js
@@ -1,10 +1,17 @@
+function dime(num){
+    if (num == 13 ){
+    return true;}
+    
+}
 
+let num = prompt('Dime un número impar entre 12 y 16 que no sea multiplo de 5');
+if (dime(num)) {
+     alert("correcto")}
+    else{
+     alert("vuelve a intentar")
+        }
     
 
-function saludar(){
-    alert("Hola, como estas?");
 
-}
-saludar();
     
 
